c6bad4118b853a2c311684539c2e5876e03e08fa
Roger Turnau <rturnau@gmail.com>
2020-11-02 21:54:17 -0500

    fixed JUnit version

--- src/test/java/com/chyernobog/cucumberdemo/FizzBuzzStepDefs.java
+++ src/test/java/com/chyernobog/cucumberdemo/FizzBuzzStepDefs.java
@@ -1,43 +1,32 @@
 package com.chyernobog.cucumberdemo;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java8.En;
 
 import static org.junit.Assert.*;
 
-public class FizzBuzzStepDefs {
-
+public class FizzBuzzStepDefs implements En {
     private FizzBuzz fizzBuzz;
 
-    @Given("FizzBuzz exists")
-    public void fizzBuzzExists() {
-        fizzBuzz = new FizzBuzz();
-    }
+    public FizzBuzzStepDefs() {
+        Given("FizzBuzz exists", () -> fizzBuzz = new FizzBuzz());
 
-    @When("FizzBuzz is running")
-    public void fizzBuzzIsRunning() {
-        fizzBuzz.runFizzBuzz();
-    }
+        When("FizzBuzz is running", () -> fizzBuzz.runFizzBuzz());
 
-    @Then("FizzBuzz counts to 100")
-    public void fizzBuzzCountsToN() {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 != 0 && i % 5 != 0) {
-                assertEquals(String.valueOf(i), fizzBuzz.getCount()[i]);
+        Then("FizzBuzz counts to 100", () -> {
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 != 0 && i % 5 != 0) {
+                    assertEquals(String.valueOf(i), fizzBuzz.getCount()[i]);
+                }
             }
-        }
-    }
+        });
 
-    @When("^(\\w+) is divisible by (\\w+)$")
-    public void countIsDivisibleByThree(String count, String three) {
-        assert(Integer.valueOf(count) % Integer.valueOf(three) == 0);
-    }
+        When("{int} is divisible by {int}", (Integer count, Integer three) -> {
+            assert(count % three == 0);
+        });
 
-    @Then("^FizzBuzz prints out (\\w+) for (\\w+)$")
-    public void fizzBuzzPrintsOutFizz(String result, String count) {
-        String res = fizzBuzz.getCount()[Integer.valueOf(count)];
-        assert(res.equalsIgnoreCase(result));
+        Then("FizzBuzz prints out {word} for {int}", (String result, Integer count) -> {
+            String res = fizzBuzz.getCount()[count];
+            assert(res.equalsIgnoreCase(result));
+        });
     }
-
 }
--- src/test/java/com/chyernobog/cucumberdemo/RunCucumberTests.java
+++ src/test/java/com/chyernobog/cucumberdemo/RunCucumberTests.java
@@ -1,13 +1,13 @@
 package com.chyernobog.cucumberdemo;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
-import cucumber.api.junit.Cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"},
-        snippets = SnippetType.CAMELCASE,
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
         glue = "com.chyernobog.cucumberdemo")
 public class RunCucumberTests {
 }
